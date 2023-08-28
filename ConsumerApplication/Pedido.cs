92b65f1056cc1702241b396ba2e4999144595f8f
Ailin Fillion <ailin.fillion@artekium.com>
2023-08-28 14:45:17 -0400

    first commit

---
+++ ConsumerApplication/Pedido.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsumerApplication
+{
+    public class Pedido
+    {
+        public Guid Id { get; set; }
+
+        public int? NumeroPedido { get; set; }
+
+        public string CicloDelPedido { get; set; } = string.Empty;
+
+        public long CodigoDeContratoInterno { get; set; }
+
+        public EstadoDelPedido EstadoDelPedido { get; set; }
+
+        public string CuentaCorriente { get; set; } = string.Empty;
+
+        public DateTime Cuando { get; set; }
+    }
+}
---
+++ ConsumerApplication/Program.cs
@@ -0,0 +1,40 @@
+﻿using Confluent.Kafka;
+using ConsumerApplication;
+using System.Text.Json;
+
+var consumerConfig = new ConsumerConfig
+{
+    GroupId = "Pedido",
+    BootstrapServers = "134.209.172.61:29093", //"localhost:29092",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+var producerConfig = new ProducerConfig
+{
+    BootstrapServers = "134.209.172.61:29093"
+};
+
+using (var consumer = new ConsumerBuilder<Null, string>(consumerConfig).Build())
+{
+    consumer.Subscribe("PedidoCreado");
+    while (true)
+    {
+        var consumeResult = consumer.Consume();
+
+        if (consumeResult != null)
+        {
+            Pedido? pedido = JsonSerializer.Deserialize<Pedido>(consumeResult.Message.Value);
+
+            Random random = new Random();
+            int numeroPedido = random.Next(100000000, 999999999);
+
+            string datosPedido = $"{pedido?.Id}/{numeroPedido}";
+
+            using (var producer = new ProducerBuilder<Null, string>(producerConfig).Build())
+            {
+                await producer.ProduceAsync("PedidoAsignado", new Message<Null, string> { Value = datosPedido });
+                producer.Flush(TimeSpan.FromSeconds(10));
+            }
+        }
+    }
+}
